274a654014346f5e619d93bcc3873a886b3c5b83
corynorris <corynorris@gmail.com>
2016-02-18 15:45:27 -0500

    Added user list, username selection, and various other features

--- index.js
+++ index.js
@@ -4,25 +4,48 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-  	console.log('a user disconnected');
-  });
-  socket.on('chat message', function(message) {
-  	socket.broadcast.emit('chat message', message);
-  });
+var users = [];
+io.on('connection', function(socket) {
+
+    var userSet = false;
+
+    socket.on('join chat', function(username) {
+  	    if (userSet) return;
+  	    userSet = true;
+        socket.username = username;
+        users.push(username);
+        socket.broadcast.emit('user joined', socket.username);
+        console.log("user joined: " + username);
+    })
+
+    socket.on('disconnect', function() {
+        var index = users.indexOf(socket.username);
+        if (index >= 0) {
+            users.splice(index, 1);
+            socket.broadcast.emit('user left', socket.username);
+            console.log("User left: " + socket.username);
+        }
+    });
+
+    socket.on('send message', function(message) {
+        socket.broadcast.emit('chat message', {
+        	author: socket.username,
+        	text: message,
+        });
+    });
+
+    socket.emit('users', users);
 });
 
 var port = process.env.PORT || 3000;
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,69 @@
 var socket = io();
 
-var vm = new Vue({
-  el: "#chat",
-  data: {
-    messages: [],
-    input: ""
-  },
-  methods: {
-    typing: function(e) {
 
-    },
-    post: function(e) {
-      if (!this.input)
-        return;
-      var message = this.input;
-      this.input = '';
-      this.messages.push(message);
-      socket.emit('chat message', message);
-      e.preventDefault();
+// register modal component
+Vue.component('modal', {
+    template: '#modal-template',
+    props: {
+        show: {
+            type: Boolean,
+            required: true,
+            twoWay: true
+        }
     }
-  }
+})
+
+var vm = new Vue({
+    el: "#chat",
+    data: {
+        showModal: true,
+        username: null,
+        messages: [],
+        users: [],
+        input: ""
+    },
+    methods: {
+        addUser: function(e) {
+            if (!this.username)
+                return;
+            this.users.push(this.username);
+            socket.emit('join chat', this.username);
+            vm.showModal = false;
+
+        },
+        sendMessage: function(e) {
+          // Make sure there is input
+            if (!this.input)
+                return;
+
+            // Update messages           
+            this.messages.push({
+              author: this.username,
+              text: this.input,
+            });
+
+            socket.emit('send message', this.input);
+
+            // Clear input for next message
+            this.input = '';         
+        }
+    }
 });
 
-socket.on('chat message', function(msg){
-  console.log('pushing message');
-  vm.messages.push(msg);
+socket.on('users', function(users) {
+    vm.users = users;
+});
+
+socket.on('chat message', function(msg) {
+    vm.messages.push(msg);
+});
+
+socket.on('user joined', function(username) {
+    vm.users.push(username);
+});
+
+
+socket.on('user left', function(username) {
+    var index = vm.users.indexOf(username);
+    vm.users.splice(index, 1);
 });
