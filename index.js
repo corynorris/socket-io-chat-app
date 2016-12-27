75b19a82f9ef9d0fa0283b1eb97f59d1076a1855
corynorris <corynorris@gmail.com>
2016-12-26 20:54:34 -0500

    fixed spelling, addes start script

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 var users = [];
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
     var userSet = false;
 
-    socket.on('join chat', function(username) {
-  	    if (userSet) return;
-  	    userSet = true;
+    socket.on('join chat', function (username) {
+        if (userSet) return;
+        userSet = true;
         socket.username = username;
         users.push(username);
         socket.broadcast.emit('user joined', socket.username);
         console.log("user joined: " + username);
     })
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         var index = users.indexOf(socket.username);
         if (index >= 0) {
             users.splice(index, 1);
             socket.broadcast.emit('user left', socket.username);
-            console.log("User left: " + socket.username);
+            console.log("user left: " + socket.username);
         }
     });
 
-    socket.on('send message', function(message) {
+    socket.on('send message', function (message) {
         socket.broadcast.emit('chat message', {
-        	author: socket.username,
-        	text: message,
+            author: socket.username,
+            text: message,
         });
     });
 
@@ -46,6 +46,6 @@ io.on('connection', function(socket) {
 
 var port = process.env.PORT || 3000;
 
-http.listen(port, function() {
+http.listen(port, function () {
     console.log('listening on *:' + port);
 });
